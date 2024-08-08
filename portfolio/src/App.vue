e266a80c0598f4074efe023e9628e511e444b57b
Matthew En <86386184+Mcen25@users.noreply.github.com>
2024-08-08 18:36:47 -0400

    Added navbar

--- portfolio/src/App.vue
+++ portfolio/src/App.vue
@@ -1,11 +1,37 @@
-<script setup lang="ts">
+<script lang="ts">
+import { defineComponent, reactive, ref, toRefs } from 'vue'
 import { RouterLink, RouterView } from 'vue-router'
+import navbar from './components/Navbar.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    navbar,
+    RouterLink,
+    RouterView
+  },
+  setup() {
+    const state = reactive({
+      name: 'Link',
+      age: 25 as string | number
+    })
+
+    return { ...toRefs(state) }
+
+    const name = ref('Link')
+  },
+  methods: {
+    changeName(name: string) {
+      this.name = name
+    }
+  }
+})
 </script>
 
 <template>
-
+  <div id="app">
+    <navbar />
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
---
+++ portfolio/src/components/Navbar.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" class="custom-navbar">
+      <b-navbar-brand href="#">Matthew En's Portfolio</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#" class="custom-navbar">Home</b-nav-item>
+          <b-nav-item href="#">About</b-nav-item>
+          <b-nav-item href="#">Projects</b-nav-item>
+          <b-nav-item href="#">Contact</b-nav-item>
+        </b-navbar-nav>
+
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item-dropdown right>
+            <b-dropdown-item href="#">Profile</b-dropdown-item>
+            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Navbar'
+})
+</script>
+
+<style scoped>
+.custom-navbar {
+  background-color: #14213d; /* Custom background color */
+  color: #e0e1dd; /* Custom text color */
+}
+
+.custom-navbar .navbar-brand,
+.custom-navbar .nav-link {
+  color: #e0e1dd; /* Custom link color */
+}
+
+.custom-navbar .nav-link:hover {
+  color: #ffd700; /* Custom hover color */
+}
+</style>
