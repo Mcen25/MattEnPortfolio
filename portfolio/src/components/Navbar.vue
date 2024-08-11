f4989a7f1aa9e8dfffde4a436de4eb6c623e0028
Matthew En <86386184+Mcen25@users.noreply.github.com>
2024-08-11 13:48:12 -0400

    nest npm install

--- portfolio/src/components/Navbar.vue
+++ portfolio/src/components/Navbar.vue
@@ -34,30 +34,6 @@ export default defineComponent({
 </script>
 
 <template>
-  <!-- <b-navbar toggleable="lg" class="navbar-color">
-    <div class="container d-flex justify-content-center">
-      <b-navbar-brand href="#"> Matthew En's Portfolio </b-navbar-brand>
-
-      <div class="d-flex justify-content-center">
-        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-        <b-collapse id="nav-collapse" is-nav>
-          <b-navbar-nav>
-            <b-nav-item href="#">Home</b-nav-item>
-            <b-nav-item href="#">About</b-nav-item>
-            <b-nav-item-dropdown text="Projects" right>
-              <b-dropdown-item href="">Multi-Agent Task in Unity</b-dropdown-item>
-              <b-dropdown-item href="#">MRI Project</b-dropdown-item>
-              <b-dropdown-item href="#">Constellation Website</b-dropdown-item>
-              <b-dropdown-item href="#">Game Jam #14 -- Spread</b-dropdown-item>
-            </b-nav-item-dropdown>
-            <b-nav-item href="#">Contact</b-nav-item>
-          </b-navbar-nav>
-        </b-collapse>
-      </div>
-    </div>
-  </b-navbar> -->
-
   <BNavbar toggleable="lg" variant="light" bg="light" class="navbar-line">
     <div class="container d-flex justify-content-center align-items-center">
       <BNavbarToggle target="nav-collapse" />
