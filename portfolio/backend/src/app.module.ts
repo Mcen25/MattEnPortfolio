0aa4f0407a848c49675c9a0ba7b7b04181cc56df
Matthew En <86386184+Mcen25@users.noreply.github.com>
2024-08-12 12:05:16 -0400

    postgres integration

--- portfolio/backend/src/app.module.ts
+++ portfolio/backend/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POST_HOST,
+      port: 5432,
+      username: process.env.POST_USER,
+      password: process.env.POST_PASSWORD,
+      database: process.env.POST_DATABASE,
+      entities: [],
+      synchronize: true,
+      logging: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
