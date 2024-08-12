d5a78352e88ec2ebd5b8f82ff9a77e0730819130
Matthew En <86386184+Mcen25@users.noreply.github.com>
2024-08-12 19:27:11 -0400

    api work

--- portfolio/backend/src/app.module.ts
+++ portfolio/backend/src/app.module.ts
@@ -1,15 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { FormModule } from './form/form.module';
-import * as dotenv from 'dotenv';
 import { Form } from './form/entities/form.entity';
-
+import { HttpAdapterHost } from '@nestjs/core';
+import * as dotenv from 'dotenv';
 dotenv.config();
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../..', 'frontend/dist'),
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.POST_HOST,
@@ -24,6 +29,6 @@ dotenv.config();
     FormModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, HttpAdapterHost],
 })
 export class AppModule {}
