e32809f8650cb49cc1c70a319d3fb46c43ee29ac
Matthew En <86386184+Mcen25@users.noreply.github.com>
2024-08-12 13:28:44 -0400

    Update to contact view
    Added in a form from boostrap-vue-next componenet

--- portfolio/frontend/src/views/ContactView.vue
+++ portfolio/frontend/src/views/ContactView.vue
@@ -36,14 +36,66 @@
     </BRow>
   </BContainer>
   <h1>Or you can reach me through this contact form:</h1>
+
+  <BForm @submit="onSubmit" @reset="onReset" v-if="show">
+    <BFormGroup
+      id="input-group-1"
+      label="Email address:"
+      label-for="input-1"
+      description="We'll never share your email with anyone else."
+    >
+      <BFormInput
+        id="input-1"
+        v-model="form.email"
+        type="email"
+        placeholder="Enter email"
+        required
+      />
+    </BFormGroup>
+
+    <BFormGroup id="input-group-2" label="Your Name:" label-for="input-2">
+      <BFormInput id="input-2" v-model="form.name" placeholder="Enter name" required />
+    </BFormGroup>
+    <BFormGroup id="input-group-3" label="Food:" label-for="input-3">
+      <BFormSelect id="input-3" v-model="form.food" :options="foods" required />
+    </BFormGroup>
+
+    <BFormGroup id="input-group-4">
+      <BFormCheckboxGroup v-model="form.checked" id="checkboxes-4">
+        <BFormCheckbox value="me">Check me out</BFormCheckbox>
+        <BFormCheckbox value="that">Check that out</BFormCheckbox>
+      </BFormCheckboxGroup>
+    </BFormGroup>
+    <BButton type="submit" variant="primary">Submit</BButton>
+    <BButton type="reset" variant="danger">Reset</BButton>
+  </BForm>
+
+  <BCard class="mt-3" header="Form Data Result">
+    <pre class="m-0">{{ form }}</pre>
+  </BCard>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import { BContainer, BRow, BCol, BCard, BCardText, BButton } from 'bootstrap-vue-next'
+import { defineComponent, nextTick } from 'vue'
+import {
+  BContainer,
+  BRow,
+  BCol,
+  BCard,
+  BButton,
+  BForm,
+  BFormGroup,
+  BFormInput,
+  BFormSelect,
+  BFormCheckboxGroup,
+  BFormCheckbox,
+  BCardText
+} from 'bootstrap-vue-next'
 
 import linkedinLogo from '../assets/LinkedIn_icon.svg.png'
 import githubLogo from '../assets/GitHub_Invertocat_Logo.svg.png'
+import { reactive } from 'vue'
+import { ref } from 'vue'
 
 export default defineComponent({
   name: 'ContactView',
@@ -52,13 +104,46 @@ export default defineComponent({
     BRow,
     BCol,
     BCard,
-    BCardText,
-    BButton
+    BButton,
+    BForm,
+    BFormGroup,
+    BFormInput,
+    BFormSelect,
+    BFormCheckboxGroup,
+    BFormCheckbox,
+    BCardText
   },
   data() {
     return {
+      foods: [{ text: 'Select One', value: null }, 'Carrots', 'Beans', 'Tomatoes', 'Corn'],
       linkedinLogo,
-      githubLogo
+      githubLogo,
+      form: {
+        email: '',
+        name: '',
+        food: null,
+        checked: []
+      },
+      show: true
+    }
+  },
+  methods: {
+    onSubmit(event: Event) {
+      event.preventDefault()
+      alert(JSON.stringify(this.form))
+    },
+    onReset(event: Event) {
+      event.preventDefault()
+      // Reset our form values
+      this.form.email = ''
+      this.form.name = ''
+      this.form.food = null
+      this.form.checked = []
+      // Trick to reset/clear native browser form validation state
+      this.show = false
+      nextTick(() => {
+        this.show = true
+      })
     }
   }
 })
